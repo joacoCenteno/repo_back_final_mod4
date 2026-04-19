e537c8e072610d05aba0b8238aaed0bde670a466
joacoCenteno <centenojoaco01@gmail.com>
2026-04-19 18:57:25 -0300

    General changes

--- controller/AuthControlles.mjs
+++ controller/AuthControlles.mjs
@@ -5,7 +5,7 @@ export async function registerController(req,res){
         const result = await AuthService.register(req.body);
         res.status(200).json(result)
     } catch (error) {
-        res.status(400).json({message: error.message})
+        res.status(400).json({message: "Usuario ya existente"})
     }
 }
 
@@ -15,6 +15,6 @@ export async function loginController(req,res){
         const result = await AuthService.login(email,password);
         res.status(200).json(result)
     } catch (error) {
-        res.status(400).json({message: error.message})
+        res.status(400).json({message: "Email o contraseña incorrectas"})
     }
 }

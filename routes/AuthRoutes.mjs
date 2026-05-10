bdb13a78be502c1d767c5fd379091bd8964dcd27
joacoCenteno <centenojoaco01@gmail.com>
2026-05-09 22:30:27 -0300

    Logout function was deleted

--- routes/AuthRoutes.mjs
+++ routes/AuthRoutes.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerController, loginController, logoutController } from '../controller/AuthControlles.mjs'
+import { registerController, loginController } from '../controller/AuthControlles.mjs'
 import {loginValidator, registerValidator} from '../middleware/validationRules.mjs'
 import { loginLimiter, registerLimiter } from '../middleware/rateLimit.mjs';
 
@@ -7,6 +7,5 @@ const router = express.Router();
 
 router.post('/register', registerValidator, registerLimiter, registerController)
 router.post('/login', loginLimiter, loginLimiter, loginController)
-router.post('/logout', logoutController)
 
 export default router;
